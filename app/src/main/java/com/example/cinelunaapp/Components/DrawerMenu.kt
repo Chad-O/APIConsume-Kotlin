64a37456341237ee43435383cda426c4086f9acd
Mauricio <mauriciocisneroslaule@gmail.com>
2022-10-08 12:49:14 -0500

    Navigation depedency agregado

--- app/src/main/java/com/example/cinelunaapp/Components/DrawerMenu.kt
+++ app/src/main/java/com/example/cinelunaapp/Components/DrawerMenu.kt
@@ -1,4 +1,44 @@
 package com.example.cinelunaapp.Components
 
-class DrawerMenu {
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import java.lang.reflect.Modifier
+
+@Composable
+fun DrawerMenu(
+    navController: NavController,
+    onCloseDrawer : () -> Unit,
+    onChangeTitle: (String)->Unit
+){
+    Column(
+        modifier=Modifier.fillMaxSize().padding(
+            start = 24.dp,
+            end = 24.dp
+        )
+    ){
+        Text(
+            //Debe ser el nombre ingresado en login
+            text = "Placeholder"
+        )
+        Divider(
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = "Cartelera",
+            modifier = Modifier.height(23.dp).clickable
+            {
+                //Agregar el lugar de navegacion
+                navController.navigate("")
+                onCloseDrawer()
+                onChangeTitle("")
+            }
+        )
+
+    }
 }
