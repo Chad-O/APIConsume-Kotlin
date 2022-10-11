8f267455373f7052939c97db206f6f8ff945929b
Mauricio <mauriciocisneroslaule@gmail.com>
2022-10-11 07:35:47 -0500

    Pelicula Card Estética

--- app/src/main/java/com/example/cinelunaapp/presentation/components/PeliculaCard.kt
+++ app/src/main/java/com/example/cinelunaapp/presentation/components/PeliculaCard.kt
@@ -1,13 +1,22 @@
 package com.example.cinelunaapp.presentation.components
 
 import android.security.identity.AccessControlProfile
+import androidx.compose.animation.AnimatedContentScope.SlideDirection.Companion.Start
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.cinelunaapp.model.Pelicula
@@ -16,23 +25,51 @@ import com.example.cinelunaapp.presentation.screens.PeliculaScreen
 @Composable
 fun PeliculaCard(pelicula: Pelicula, navigateToProfile: (Pelicula)->Unit){
     Row(modifier = Modifier
-        .padding(all = 8.dp)
-        .clickable{navigateToProfile(pelicula)}
+        .clickable { navigateToProfile(pelicula) }
         .fillMaxWidth()
     ) {
-        Column {
-            Text(
-                text = pelicula.nombre,
-                color = MaterialTheme.colors.secondaryVariant,
-                style = MaterialTheme.typography.subtitle2
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = pelicula.hora,
-                modifier = Modifier.padding(all = 4.dp ),
-                style = MaterialTheme.typography.subtitle2
+        //Columna que declara el divisor
+        Column() {
+            //Row para ordenar dos columnas una "Estrella" y otra la "pelicula"
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier.padding(start = 0.dp, top = 5.dp, end = 0.dp, bottom = 5.dp)
+            ){
+                //Estrella
+                Column() {
+                    Icon(
+                        imageVector = Icons.Filled.Star,
+                        contentDescription = "Star" ,
+                        modifier = Modifier.padding(start = 3.dp, top = 10.dp, end = 0.dp, bottom = 0.dp)
+                            .size(40.dp)
+                    )
+                }
+                //Pelicula
+                Column() {
+                    Text(
+                        text = pelicula.nombre,
+                        color = MaterialTheme.colors.secondaryVariant,
+                        style = MaterialTheme.typography.subtitle2,
+                        modifier = Modifier.padding(start = 8.dp, top = 10.dp, end = 0.dp, bottom = 0.dp),
+                        fontSize = 20.sp
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = pelicula.hora,
+                        modifier = Modifier.padding(start = 8.dp, top = 2.dp, end = 0.dp, bottom = 0.dp),
+                        style = MaterialTheme.typography.subtitle2,
+                        fontSize = 18.sp
+                    )
+
+                }
+            }
+            Divider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(2.dp, Color.Black)
             )
         }
+
         //NavigationGraph(navController = navController, username ="" )
     }
 
